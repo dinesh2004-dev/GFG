47eb86c222357cb8fb3736113af4aa2aeb2b6ecc
Dinesh Reddy <dineshreddy2295@gmail.com>
2025-12-03 17:15:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.java
+++ Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.java
@@ -12,85 +12,95 @@ class Node {
 
 class Solution {
     public boolean isLeaf(Node root){
-    
-    return root.left == null && root.right == null;
-}
-public void addLeft(Node root,ArrayList<Integer> boundaryNodes){
-    
-    Node curr = root.left;
-    
-    while(curr != null){
         
-        if(!isLeaf(curr)){
-            
-            boundaryNodes.add(curr.data);
-        }
-        if(curr.left != null){
-            
-            curr = curr.left;
-        }
-        else{
-            
-            curr = curr.right;
-        }
+        return root.left == null && root.right == null;
     }
-}
-public void addRight(Node root,ArrayList<Integer> boundaryNodes){
-    
-    ArrayList<Integer> temp = new ArrayList<>();
-    
-    Node curr = root.right;
-    
-    while(curr != null){
+    public void addLeft(Node root,List<Integer> res){
         
-        if(!isLeaf(curr)){
+        Node curr = root.left;
+        
+        while(curr != null){
             
-            temp.add(curr.data);
-        }
-        if(curr.right != null){
-            
-            curr = curr.right;
-        }
-        else{
-            curr = curr.left;
+            if(!isLeaf(curr)){
+                
+                res.add(curr.data);
+            }
+            if(curr.left != null){
+                
+                curr = curr.left;
+            }
+            else{
+                
+                curr = curr.right;
+            }
         }
     }
     
-    for(int i = temp.size() - 1; i >= 0; i--){
+    public void addLeaf(Node root,List<Integer> res){
         
-        boundaryNodes.add(temp.get(i));
-    }
-}
-public void addBottom(Node root,ArrayList<Integer> boundaryNodes){
-    if(root == null){
-        return;
+        if(root == null){
+            
+            return;
+        }
+        if(isLeaf(root)){
+            
+            res.add(root.data);
+        }
+        
+        addLeaf(root.left,res);
+        addLeaf(root.right,res);
     }
     
-    if(isLeaf(root)){
+    public void addRight(Node root,List<Integer> res){
         
-        boundaryNodes.add(root.data);
+        Node curr = root.right;
+        
+        List<Integer> temp = new ArrayList<>();
+        
+        while(curr != null){
+            
+            if(!isLeaf(curr)){
+                
+                temp.add(curr.data);
+            }
+            if(curr.right != null){
+                
+                curr = curr.right;
+            }
+            else{
+                
+                curr = curr.left;
+            }
+        }
+        
+        for(int i = temp.size() - 1; i >= 0; i--){
+            
+            res.add(temp.get(i));
+        }
         
     }
-    
-    addBottom(root.left,boundaryNodes);
-    addBottom(root.right,boundaryNodes);
-}
     ArrayList<Integer> boundaryTraversal(Node root) {
         // code here
-        ArrayList<Integer> boundaryNodes = new ArrayList<>();
+        if(root == null){
+            
+            return new ArrayList<>();
+        }
         
-        boundaryNodes.add(root.data);
+        
+        ArrayList<Integer> res = new ArrayList<>();
+        
+        res.add(root.data);
         
         if(isLeaf(root)){
             
-            return boundaryNodes;
+            return res;
+            
         }
         
-        addLeft(root,boundaryNodes);
-        addBottom(root,boundaryNodes);
-        addRight(root,boundaryNodes);
-       
+        addLeft(root,res);
+        addLeaf(root,res);
+        addRight(root,res);
         
-        return boundaryNodes;
+        return res;
     }
 }
