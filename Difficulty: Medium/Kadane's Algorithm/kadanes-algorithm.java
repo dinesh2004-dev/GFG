b006948f7bb423a669e649a27ce447d8ce0bf11a
Dinesh Reddy <dineshreddy2295@gmail.com>
2025-12-03 21:07:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
+++ Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
@@ -2,35 +2,22 @@ class Solution {
     int maxSubarraySum(int[] arr) {
         // Code here
         
-        int sum = 0;
         int n = arr.length;
         
-        if(n == 1){
-            
-            return arr[0];
-        }
+        int sum = 0;
         
-        int maxSum = 0;
-        int countNeg = 0;
+        int maxSum = Integer.MIN_VALUE;
         
         for(int i = 0; i < n; i++){
             
-            if(arr[i] < 0){
-                countNeg++;
-            }
-            
-            sum += arr[i];
-            
-            maxSum = Math.max(sum,maxSum);
             if(sum < 0){
                 
                 sum = 0;
             }
-        }
-        
-        if(countNeg == n){
             
-            maxSum = Arrays.stream(arr).max().getAsInt();
+            sum = sum + arr[i];
+            
+            maxSum = Math.max(maxSum,sum);
         }
         
         return maxSum;
